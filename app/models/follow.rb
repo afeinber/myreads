7dc811a9b172898d37474e37987242343cd9bc9b
afeinber <alexandernfeinberg@gmail.com>
2014-07-10 12:13:24 -0400

    Requests are deleted when responded to with a confirmation. Also fixes some issues with comments being nil.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,4 +4,12 @@ class Follow < ActiveRecord::Base
 
   validates! :user_id, uniqueness: {scope: :followee_id}
   validates! :user_id, exclusion: { in: [:followee_id] }
+
+  before_create(:delete_requests)
+
+
+  def delete_requests
+    rqst = self.user.inverse_requests.find_by(user: self.followee)
+    rqst.destroy if rqst.present?
+  end
 end
