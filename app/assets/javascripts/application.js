a564ced79a2f389a4b3e64c11eb5d63716e80bf2
afeinber <alexandernfeinberg@gmail.com>
2014-07-13 20:18:54 -0400

    changed javacript again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,10 @@
 
 
 
-//= require bootstrap-sprockets
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require magnific-popup
 //= require_tree .
