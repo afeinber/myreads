08ca159f7b9ee443d89e44eee841ef5a73b4f01f
afeinber <alexandernfeinberg@gmail.com>
2014-07-13 20:26:37 -0400

    got the popups working

--- app/assets/javascripts/myreads.js
+++ app/assets/javascripts/myreads.js
@@ -8,12 +8,3 @@ $('.open-popup-link').magnificPopup({
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 
-// // Example: 2 Dynamically created
-// $('.user_book').magnificPopup({
-//   items: {
-//       src: '<div class="white-popup">Dynamically created popup</div>',
-//       type: 'inline'
-//   },
-//   closeBtnInside: true
-//   midClick: true
-// });
