c6aab35687de0e1a1829b6d5d264f5cbf2fb2064
afeinber <alexandernfeinberg@gmail.com>
2014-07-15 09:14:55 -0400

    removed a binding.pry

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -18,10 +18,8 @@ class RequestsController < ApplicationController
   def index
     if current_user.inverse_recommendations.count > 0
 
-      @books = current_user.inverse_recommendations.map(&:book)
-      @books = @books.zip(current_user.inverse_recommendations.map(&:user))
-      binding.pry
-      @books.to_h
+      @books = current_user.inverse_recommendations.map(&:book).
+        zip(current_user.inverse_recommendations.map(&:user)).to_h
     end
     @users = current_user.inverse_requests.map(&:user)
 
